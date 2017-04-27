b0cfb5685310c1920af19a08d103c566fc88a00c
Asim Aslam <asim@chuhnk.me>
2017-04-27 19:57:53 +0100

    Add example of bidirectional stream and server stream

--- stream/client/main.go
+++ stream/client/main.go
@@ -8,13 +8,7 @@ import (
 	"golang.org/x/net/context"
 )
 
-func main() {
-	service := micro.NewService()
-	service.Init()
-
-	// create client
-	cl := proto.NewStreamerClient("go.micro.srv.stream", service.Client())
-
+func bidirectional(cl proto.StreamerClient) {
 	// create streaming client
 	stream, err := cl.Stream(context.Background())
 	if err != nil {
@@ -22,6 +16,7 @@ func main() {
 		return
 	}
 
+	// bidirectional stream
 	// send and receive messages for a 10 count
 	for j := 0; j < 10; j++ {
 		if err := stream.Send(&proto.Request{Count: int64(j)}); err != nil {
@@ -40,4 +35,43 @@ func main() {
 	if err := stream.Close(); err != nil {
 		fmt.Println("stream close err:", err)
 	}
+}
+
+func serverStream(cl proto.StreamerClient) {
+	// send request to stream count of 10
+	stream, err := cl.ServerStream(context.Background(), &proto.Request{Count: int64(10)})
+	if err != nil {
+		fmt.Println("err:", err)
+		return
+	}
+
+	// server side stream
+	// receive messages for a 10 count
+	for j := 0; j < 10; j++ {
+		rsp, err := stream.Recv()
+		if err != nil {
+			fmt.Println("recv err", err)
+			break
+		}
+		fmt.Printf("got msg %v\n", rsp.Count)
+	}
+
+	// close the stream
+	if err := stream.Close(); err != nil {
+		fmt.Println("stream close err:", err)
+	}
+}
+
+func main() {
+	service := micro.NewService()
+	service.Init()
+
+	// create client
+	cl := proto.NewStreamerClient("go.micro.srv.stream", service.Client())
+
+	// bidirectional stream
+	bidirectional(cl)
+
+	// server side stream
+	serverStream(cl)
 }
--- stream/server/main.go
+++ stream/server/main.go
@@ -11,6 +11,18 @@ import (
 
 type Streamer struct{}
 
+// Server side stream
+func (e *Streamer) ServerStream(ctx context.Context, req *proto.Request, stream proto.Streamer_ServerStreamStream) error {
+	log.Printf("Got msg %v", req.Count)
+	for i := 0; i < int(req.Count); i++ {
+		if err := stream.Send(&proto.Response{Count: int64(i)}); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Bidirectional stream
 func (e *Streamer) Stream(ctx context.Context, stream proto.Streamer_StreamStream) error {
 	for {
 		req, err := stream.Recv()
