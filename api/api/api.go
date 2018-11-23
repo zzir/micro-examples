5c0726467e0d4e7551e463ffd577c75ab3762ce9
Asim Aslam <asim@aslam.me>
2018-11-23 17:18:01 +0000

    fix broken build

--- api/api/api.go
+++ api/api/api.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"strings"
 
-	proto "github.com/micro/examples/api/default/proto"
+	proto "github.com/micro/examples/api/api/proto"
 	api "github.com/micro/go-api/proto"
 	"github.com/micro/go-micro"
 	"github.com/micro/go-micro/errors"
